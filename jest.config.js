2b0bc39ac9b5c4353a53e2940f46064887c5da7b
Grigorii K. Shartsev <me@shgk.me>
2021-06-24 22:05:55 +0500

    fix(taskbook): fix jest config to support lodash-es

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ const config = {
   preset: '@vue/cli-plugin-unit-jest',
   modulePaths: ['utility_modules'],
   testMatch: ['**/__tests__/**/*.(spec|test|student-test).[jt]s?(x)'],
+  moduleNameMapper: {
+    // Crutch: Use commonjs version of lodash in tests
+    '^lodash-es$': '<rootDir>/node_modules/lodash/index.js',
+  },
 };
 
 if (process.env.TASK_MONITOR) {
