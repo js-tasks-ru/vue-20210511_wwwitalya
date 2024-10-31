e6ee4711882119e176dc2d6e8a3e590809944eba
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-10-31 09:06:49 +0300

    Added task Meetup Page Draft

---
+++ 01-basics/04-MeetupPageDraft/script.js
@@ -0,0 +1,47 @@
+import Vue from './vendor/vue.esm.browser.js';
+
+/** URL адрес API */
+const API_URL = 'https://course-vue.javascript.ru/api';
+
+/** ID митапа для примера; используйте его при получении митапа */
+const MEETUP_ID = 6;
+
+/**
+ * Возвращает ссылку на изображение по идентификатору, например, изображение митапа
+ * @param imageId {number} - идентификатор изображения
+ * @return {string} - ссылка на изображение
+ */
+function getImageUrlByImageId(imageId) {
+  return `${API_URL}/images/${imageId}`;
+}
+
+/**
+ * Словарь заголовков по умолчанию для всех типов пунктов программы
+ */
+const agendaItemDefaultTitles = {
+  registration: 'Регистрация',
+  opening: 'Открытие',
+  break: 'Перерыв',
+  coffee: 'Coffee Break',
+  closing: 'Закрытие',
+  afterparty: 'Afterparty',
+  talk: 'Доклад',
+  other: 'Другое',
+};
+
+/**
+ * Словарь иконок для для всех типов пунктов программы.
+ * Соответствует имени иконок в директории /assets/icons
+ */
+const agendaItemIcons = {
+  registration: 'key',
+  opening: 'cal-sm',
+  talk: 'tv',
+  break: 'clock',
+  coffee: 'coffee',
+  closing: 'key',
+  afterparty: 'cal-sm',
+  other: 'cal-sm',
+};
+
+// Требуется создать Vue приложение
