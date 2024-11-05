04c42536de664fc37bda932d0ccc64e21f523e63
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-11-05 06:21:52 +0300

    Added task Компонент CounterButton

---
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -0,0 +1,14 @@
+const CounterButton = {
+  name: 'CounterButton',
+
+  // Компонент должен иметь входной параметр
+
+  // Компонент должен иметь модель
+
+  // Шаблон лучше держать максимально простым, а логику выносить в методы
+
+  // Шаблон потребуется отредактировать
+  template: `<button type="button"></button>`,
+};
+
+export default CounterButton;
