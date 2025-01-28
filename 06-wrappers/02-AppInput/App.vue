5f85231805f87083b75ccf8bd9352e17c2b456cf
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-01-28 13:12:02 +0300

    Added task AppInput

---
+++ 06-wrappers/02-AppInput/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app" class="page container">
+    <button type="button" @click="$refs['firstInput'].$refs['input'].focus()">Focus first input</button>
+    <span v-if="focused"> Focused</span>
+    <p><app-input ref="firstInput" v-model="value" @focus="focused = true" @blur="focused = false" /></p>
+    <p>Small <app-input v-model="value" small /></p>
+    <p>Rounded <app-input v-model="value" rounded /></p>
+    <p>Password <app-input v-model="value" type="password" /></p>
+    <p>Textarea <app-input v-model="value" multiline /></p>
+    <p>
+      With Icons <button @click="showIcons = !showIcons">Toggle Icons</button>
+      <app-input v-model="value">
+        <template v-if="showIcons" #left-icon>
+          <app-icon icon="search" />
+        </template>
+        <template v-if="showIcons" #right-icon>
+          <app-icon icon="pen-tool" />
+        </template>
+      </app-input>
+    </p>
+    <p>
+      Value + Input
+      <app-input :value="value" @input="value = $event" />
+    </p>
+    <p>
+      Value + Change
+      <app-input :value="value" @change="value = $event" />
+    </p>
+  </div>
+</template>
+
+<script>
+import AppInput from './components/AppInput';
+import AppIcon from './components/AppIcon';
+
+export default {
+  name: 'App',
+
+  components: { AppIcon, AppInput },
+
+  data() {
+    return {
+      value: 'value',
+      showIcons: true,
+      focused: false,
+    };
+  },
+};
+</script>
+
+<style>
+p {
+  margin: 1rem auto;
+}
+</style>
---
+++ 06-wrappers/02-AppInput/__tests__/AppInput.test.js
@@ -0,0 +1,150 @@
+const { shallowMount } = require('@vue/test-utils');
+const { getSolutionPath } = require('taskbook-test-utils');
+const AppInput = require(getSolutionPath('components/AppInput.vue')).default;
+
+const findInput = (wrapper) => wrapper.findComponent({ ref: 'input' });
+
+describe('wrappers/AppInput', () => {
+  describe('AppInput', () => {
+    it('AppInput должен иметь параметры value, small, rounded и multiline', () => {
+      const wrapper = shallowMount(AppInput);
+      expect(wrapper.vm.$options.props.value).toBeTruthy();
+      expect(wrapper.vm.$options.props.small).toBeTruthy();
+      expect(wrapper.vm.$options.props.rounded).toBeTruthy();
+      expect(wrapper.vm.$options.props.multiline).toBeTruthy();
+    });
+
+    it('AppInput должен рендерить поле ввода с ref="input"', () => {
+      const wrapper = shallowMount(AppInput);
+      expect(wrapper.findComponent({ ref: 'input' }).exists()).toBe(true);
+    });
+
+    it('AppInput должен рендерить поле ввода с классом form-control внутри блока input-group', () => {
+      const wrapper = shallowMount(AppInput);
+      expect(wrapper.find('.input-group').exists()).toBe(true);
+      expect(wrapper.find('.input-group input.form-control').exists()).toBe(true);
+    });
+
+    it('AppInput должен иметь класс form-control_sm только с параметром small', async () => {
+      const wrapper = shallowMount(AppInput);
+      expect(findInput(wrapper).classes('form-control_sm')).toBe(false);
+      await wrapper.setProps({ small: true });
+      expect(findInput(wrapper).classes('form-control_sm')).toBe(true);
+    });
+
+    it('AppInput должен иметь класс form-control_rounded только с параметром rounded', async () => {
+      const wrapper = shallowMount(AppInput);
+      expect(findInput(wrapper).classes('form-control_rounded')).toBe(false);
+      await wrapper.setProps({ rounded: true });
+      expect(findInput(wrapper).classes('form-control_rounded')).toBe(true);
+    });
+
+    it('AppInput с параметром multiline должен рендерить многострочное поле ввода', () => {
+      const wrapper = shallowMount(AppInput, {
+        propsData: { multiline: true },
+      });
+      expect(findInput(wrapper).exists()).toBe(true);
+      expect(findInput(wrapper).element.tagName).toBe('TEXTAREA');
+    });
+
+    it('Параметры small и rounded должны работать с многострочным AppInput', async () => {
+      const wrapper = shallowMount(AppInput, {
+        propsData: { multiline: true },
+      });
+      const textarea = findInput(wrapper);
+      expect(textarea.classes('form-control_rounded')).toBe(false);
+      expect(textarea.classes('form-control_sm')).toBe(false);
+      await wrapper.setProps({ rounded: true, small: true });
+      expect(textarea.classes('form-control_rounded')).toBe(true);
+      expect(textarea.classes('form-control_sm')).toBe(true);
+    });
+
+    it('AppInput должен принимать различные атрибуты полей ввода', () => {
+      const attrs = {
+        placeholder: 'placeholder',
+        id: 'id',
+        maxlength: '100',
+      };
+
+      const wrapper = shallowMount(AppInput, {
+        attrs,
+      });
+      expect(wrapper.find('.form-control').attributes()).toMatchObject(attrs);
+      expect(wrapper.find('.input-group').attributes()).not.toMatchObject(attrs);
+    });
+
+    it('AppInput должен принимать различные атрибуты полей ввода для textarea', () => {
+      const attrs = {
+        placeholder: 'placeholder',
+        id: 'id',
+        rows: '3',
+        cols: '4',
+      };
+
+      const wrapper = shallowMount(AppInput, {
+        propsData: { multiline: true },
+        attrs,
+      });
+      expect(wrapper.find('.form-control').attributes()).toMatchObject(attrs);
+      expect(wrapper.find('.input-group').attributes()).not.toMatchObject(attrs);
+    });
+
+    it.each(['password', 'num', 'range', 'date', 'time', 'email', 'tel'])(
+      'AppInput должен рендерить input type=(%s)',
+      (type) => {
+        const wrapper = shallowMount(AppInput, {
+          attrs: {
+            type,
+          },
+        });
+        expect(findInput(wrapper).attributes('type')).toBe(type);
+      },
+    );
+
+    it.each([false, true])('AppInput должен выводить текущее значение (multiline=%s)', async (multiline) => {
+      const value = 'SampleText';
+      const wrapper = shallowMount(AppInput, { propsData: { multiline } });
+      await wrapper.setProps({ value });
+      expect(findInput(wrapper).element.value).toBe(value);
+    });
+
+    it('AppInput должен обрабатывать нативный ввод и порождать события input, change с введённым значением', async () => {
+      const value = 'SampleText';
+      const wrapper = shallowMount(AppInput);
+      const formControl = findInput(wrapper);
+      await formControl.setValue(value);
+      await formControl.trigger('change');
+      expect(wrapper.emitted().input).toBeDefined();
+      expect(wrapper.emitted().input).toHaveLength(1);
+      expect(wrapper.emitted().input[0]).toEqual([value]);
+      expect(wrapper.emitted().change).toBeDefined();
+      expect(wrapper.emitted().change[0]).toEqual([value]);
+    });
+
+    it('AppInput должен выводить левую иконку и добавлять классы input-group_icon input-group_icon-left', async () => {
+      const wrapper = shallowMount(AppInput, {
+        slots: { 'left-icon': '<img class="icon icon-search" />' },
+      });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find('.input-group').classes()).toEqual([
+        'input-group',
+        'input-group_icon',
+        'input-group_icon-left',
+      ]);
+      expect(wrapper.find('img.icon-search + input').exists()).toBe(true);
+    });
+
+    it('AppInput должен выводить правую иконку и добавлять классы input-group_icon input-group_icon-right', async () => {
+      const wrapper = shallowMount(AppInput, {
+        slots: { 'right-icon': '<img class="icon icon-search" />' },
+      });
+      await wrapper.vm.$nextTick();
+      expect(wrapper.find('.input-group').classes()).toEqual([
+        'input-group',
+        'input-group_icon',
+        'input-group_icon-right',
+      ]);
+      expect(wrapper.find('input + img.icon-search').exists()).toBe(true);
+    });
+  });
+});
