013117ec3ffa1098922155835127002ce8b13396
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-03-20 09:29:47 +0300

    Added solution for task: CounterButton_last_final

--- 07-forms/01-CounterButton_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_final/components/CounterButton.vue
@@ -1,9 +1,43 @@
 <template>
-  <button></button>
+  <button type="button" @click="increment">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  data() {
+    return {
+      localCount: undefined,
+    };
+  },
+
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+        this.localCount = newValue;
+      },
+    },
+  },
+
+  methods: {
+    increment() {
+      this.localCount += 1;
+      this.$emit('increment', this.localCount);
+    },
+  },
 };
 </script>
