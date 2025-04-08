a3d355a78584324ffe700c5f0893d536f0e7af7e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-04-08 08:35:58 +0300

    Added task GeneratedForm

---
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <DIV class="form-section form-section_inner">
+    <button type="button" class="remove-button">
+      <app-icon icon="trash" />
+    </button>
+
+    <form-group>
+      <dropdown-button title="Тип" :options="$options.agendaItemTypeOptions" />
+    </form-group>
+
+    <div class="form__row">
+      <div class="form__col">
+        <form-group label="Начало">
+          <app-input type="time" placeholder="00:00" />
+        </form-group>
+      </div>
+      <div class="form__col">
+        <form-group label="Окончание">
+          <app-input type="time" placeholder="00:00" />
+        </form-group>
+      </div>
+    </div>
+
+    <form-group label="Заголовок">
+      <app-input />
+    </form-group>
+    <form-group label="Описание">
+      <app-input multiline />
+    </form-group>
+    <form-group>
+      <dropdown-button title="Язык" :options="$options.languageOptions" />
+    </form-group>
+  </DIV>
+</template>
+
+<script>
+import AppInput from './AppInput';
+import DropdownButton from './DropdownButton';
+import AppIcon from './AppIcon';
+import FormGroup from './FormGroup';
+import {
+  getAgendaItemsFieldSpecifications,
+  getAgendaItemTypeOptions,
+  getAgendaItemLanguageOptions,
+} from '../MeetupService';
+
+export default {
+  name: 'MeetupAgendaItemForm',
+
+  components: { FormGroup, AppIcon, AppInput, DropdownButton },
+
+  agendaItemTypeOptions: getAgendaItemTypeOptions(),
+  fieldSpecifications: getAgendaItemsFieldSpecifications(),
+  languageOptions: getAgendaItemLanguageOptions(),
+};
+</script>
+
+<style></style>
