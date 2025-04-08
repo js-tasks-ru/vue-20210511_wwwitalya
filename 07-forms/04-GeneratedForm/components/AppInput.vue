a3beb943cb3cf06c885d6a1f19b7b95db8e6183b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-04-08 11:20:19 +0300

    Added solution for task: GeneratedForm

--- 07-forms/04-GeneratedForm/components/AppInput.vue
+++ 07-forms/04-GeneratedForm/components/AppInput.vue
@@ -1,9 +1,6 @@
 <template>
-  <div
-    class="input-group"
-    :class="{ 'input-group_icon': !!$slots['left-icon'], 'input-group_icon-left': !!$slots['left-icon'] }"
-  >
-    <slot name="left-icon" />
+  <div class="input-group" :class="{ 'input-group_icon': hasIcon, 'input-group_icon-left': hasIcon }">
+    <slot name="left-icon"></slot>
     <textarea v-if="multiline" class="form-control" :value="value" v-bind="$attrs" v-on="listeners"></textarea>
     <input v-else class="form-control" :value="value" v-bind="$attrs" v-on="listeners" />
   </div>
@@ -21,8 +18,14 @@ export default {
   },
 
   props: {
-    value: String,
+    value: {},
     multiline: Boolean,
+  },
+
+  data() {
+    return {
+      hasIcon: false,
+    };
   },
 
   computed: {
@@ -32,6 +35,20 @@ export default {
         input: ($event) => this.$emit('input', $event.target.value),
         change: ($event) => this.$emit('change', $event.target.value),
       };
+    },
+  },
+
+  mounted() {
+    this.updateHasIcon();
+  },
+
+  updated() {
+    this.updateHasIcon();
+  },
+
+  methods: {
+    updateHasIcon() {
+      this.hasIcon = !!this.$slots['left-icon'];
     },
   },
 };
--- 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -1,58 +1,111 @@
 <template>
-  <DIV class="form-section form-section_inner">
-    <button type="button" class="remove-button">
+  <div class="form-section form-section_inner">
+    <button type="button" class="remove-button" @click="$emit('remove')">
       <app-icon icon="trash" />
     </button>
 
     <form-group>
-      <dropdown-button title="Тип" :options="$options.agendaItemTypeOptions" />
+      <dropdown-button v-model="localAgendaItem.type" title="Тип" :options="$options.agendaItemTypeOptions" />
     </form-group>
 
     <div class="form__row">
       <div class="form__col">
         <form-group label="Начало">
-          <app-input type="time" placeholder="00:00" />
+          <app-input v-model="localAgendaItem.startsAt" type="time" placeholder="00:00" />
         </form-group>
       </div>
       <div class="form__col">
         <form-group label="Окончание">
-          <app-input type="time" placeholder="00:00" />
+          <app-input v-model="localAgendaItem.endsAt" type="time" placeholder="00:00" />
         </form-group>
       </div>
     </div>
 
-    <form-group label="Заголовок">
-      <app-input />
+    <form-group
+      v-for="specification in $options.fieldSpecifications[localAgendaItem.type]"
+      :key="specification.field"
+      :label="specification.title"
+    >
+      <component
+        :is="specification.component"
+        :[specification.model.prop]="localAgendaItem[specification.field]"
+        v-bind="specification.props"
+        @[specification.model.event]="localAgendaItem[specification.field] = $event"
+      />
     </form-group>
-    <form-group label="Описание">
-      <app-input multiline />
-    </form-group>
-    <form-group>
-      <dropdown-button title="Язык" :options="$options.languageOptions" />
-    </form-group>
-  </DIV>
+  </div>
 </template>
 
 <script>
+import FormGroup from './FormGroup';
 import AppInput from './AppInput';
 import DropdownButton from './DropdownButton';
 import AppIcon from './AppIcon';
-import FormGroup from './FormGroup';
-import {
-  getAgendaItemsFieldSpecifications,
-  getAgendaItemTypeOptions,
-  getAgendaItemLanguageOptions,
-} from '../MeetupService';
+import { getAgendaItemsFieldSpecifications, getAgendaItemTypeOptions } from '../MeetupService';
 
 export default {
   name: 'MeetupAgendaItemForm',
 
-  components: { FormGroup, AppIcon, AppInput, DropdownButton },
+  components: { AppIcon, FormGroup, AppInput, DropdownButton },
 
   agendaItemTypeOptions: getAgendaItemTypeOptions(),
   fieldSpecifications: getAgendaItemsFieldSpecifications(),
-  languageOptions: getAgendaItemLanguageOptions(),
+
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      localAgendaItem: { ...this.agendaItem },
+    };
+  },
+
+  computed: {
+    startsAt() {
+      return this.localAgendaItem.startsAt;
+    },
+  },
+
+  watch: {
+    localAgendaItem: {
+      deep: true,
+      handler() {
+        this.$emit('update:agendaItem', { ...this.localAgendaItem });
+      },
+    },
+
+    startsAt(newValue, oldValue) {
+      // Если время не введено или введено не до конца, браузер вернёт пустую строку (при поддержке time)
+      // Но Safari не поддерживает input[type=time] :(
+      // Придётся проверять
+      if (!/([0-1]\d|2[0-3]):[0-5]\d/.test(newValue)) {
+        return;
+      }
+      // Разделяем время на часы и минуты и переводим в минуты
+      const timeToMinutes = (time) => {
+        const [h, m] = time.split(':').map((x) => parseInt(x, 10));
+        return h * 60 + m;
+      };
+      const newMinutes = timeToMinutes(newValue);
+      const oldMinutes = timeToMinutes(oldValue);
+      const oldEndsAtMinutes = timeToMinutes(this.localAgendaItem.endsAt);
+      // Считаем изменение времени в минутах
+      const deltaMinutes = newMinutes - oldMinutes;
+      // Считаем новое значение
+      const newEndsAtMinutes = (oldEndsAtMinutes + deltaMinutes + 24 * 60) % (24 * 60);
+      // Пересчитываем обратно в часы и минуты
+      const hours = Math.floor(newEndsAtMinutes / 60)
+        .toString()
+        .padStart(2, '0');
+      const minutes = Math.floor(newEndsAtMinutes % 60)
+        .toString()
+        .padStart(2, '0');
+      this.localAgendaItem.endsAt = `${hours}:${minutes}`;
+    },
+  },
 };
 </script>
-
-<style></style>
