8b2acdf34aaab7098c034c2bf17e7fa99d662224
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-04-15 11:37:36 +0300

    Added solution for task: NodesPagination

--- 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
+++ 08-deep-vue/02-NodesPagination/components/NodesPagination.vue
@@ -1,5 +1,30 @@
 <script>
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: Number,
+    perPage: Number,
+  },
+
+  render() {
+    const start = (this.page - 1) * this.perPage;
+    const end = this.page * this.perPage;
+
+    const content = this.$slots.default ?? [];
+
+    return <div>{content.slice(start, end)}</div>;
+  },
+
+  /* Или без JSX
+  render(h) {
+    const start = (this.page - 1) * this.perPage;
+    const end = this.page * this.perPage;
+
+    const content = this.$slots.default ?? [];
+
+    return h('div', content.slice(start, end));
+  },
+  */
 };
 </script>
